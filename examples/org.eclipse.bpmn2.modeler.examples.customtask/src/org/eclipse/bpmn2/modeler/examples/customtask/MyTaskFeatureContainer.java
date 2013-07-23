a76573ff8633a36e9a850526c22c677f08f7ac7d
Bob Brodt <bbrodt@redhat.com>
2013-07-23 11:46:02 -0600

    Updated custom task example to illustrate usage of BoundaryEvent extension.

--- examples/org.eclipse.bpmn2.modeler.examples.customtask/src/org/eclipse/bpmn2/modeler/examples/customtask/MyTaskFeatureContainer.java
+++ examples/org.eclipse.bpmn2.modeler.examples.customtask/src/org/eclipse/bpmn2/modeler/examples/customtask/MyTaskFeatureContainer.java
@@ -7,15 +7,25 @@ import org.eclipse.emf.ecore.EStructuralFeature;
 
 public class MyTaskFeatureContainer extends CustomTaskFeatureContainer {
 
+	private final static String TYPE_VALUE = "MyTask";
+	private final static String CUSTOM_TASK_ID = "org.eclipse.bpmn2.modeler.examples.customtask.customTask1";
+
 	public MyTaskFeatureContainer() {
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public String getId(EObject object) {
+		// This is where we inspect the object to determine what its custom task ID should be.
+		// In this case, the "type" attribute will have a value of "MyTask".
+		// If found, return the CUSTOM_TASK_ID string.
+		//
+		// Note that the object inspection can be arbitrarily complex and may include several
+		// object features. This simple case just demonstrates what needs to happen here.
 		EStructuralFeature f = ModelUtil.getAnyAttribute(object, "type");
 		if (f!=null) {
-			return this.id;
+			Object id = object.eGet(f);
+			if (TYPE_VALUE.equals(id))
+				return CUSTOM_TASK_ID;
 		}
 			
 		return null;
