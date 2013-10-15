db7a971c6b6ae4046c41db1abf319d0474adc97e
Bob Brodt <bbrodt@redhat.com>
2013-10-15 12:33:56 -0600

    Upversion to 1.0M1 (replaces version 0.3.0, which has been deleted)

--- upversion.sh
+++ upversion.sh
@@ -1,1 +1,1 @@
-mvn -Dtycho.mode=maven org.sonatype.tycho:tycho-versions-plugin:set-version -DnewVersion=0.3.0-SNAPSHOT
+mvn -Dtycho.mode=maven org.sonatype.tycho:tycho-versions-plugin:set-version -DnewVersion=1.0M1-SNAPSHOT
