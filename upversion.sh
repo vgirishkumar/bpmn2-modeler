794c3b4523b4125acea9134b2a7274ec1b3f3c0a
Bob Brodt <bbrodt@redhat.com>
2013-12-13 06:17:20 -0700

    Bug 423973 - Eclipse Luna compatibility - upversion to 1.0.2 for Luna

--- upversion.sh
+++ upversion.sh
@@ -1,1 +1,1 @@
-mvn -Dtycho.mode=maven org.sonatype.tycho:tycho-versions-plugin:set-version -DnewVersion=1.0.1-SNAPSHOT
+mvn -Dtycho.mode=maven org.sonatype.tycho:tycho-versions-plugin:set-version -DnewVersion=1.0.2-SNAPSHOT
