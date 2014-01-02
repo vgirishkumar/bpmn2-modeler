749a02a5e43da77ad0388d920659aaec557cfee3
Bob Brodt <bbrodt@redhat.com>
2014-01-02 12:56:24 -0700

    Bug 422575 - intermediate catch events validation fails. Add code to update ItemDefinition for Activity and Event DataInputs/DataOutputs if the type of ItemAwareElement that they are mapped to is changed.

--- org.eclipse.bpmn2.modeler.ui/src/org/eclipse/bpmn2/modeler/ui/adapters/properties/ItemDefinitionRefFeatureDescriptor.java
+++ org.eclipse.bpmn2.modeler.ui/src/org/eclipse/bpmn2/modeler/ui/adapters/properties/ItemDefinitionRefFeatureDescriptor.java
@@ -16,16 +16,25 @@ package org.eclipse.bpmn2.modeler.ui.adapters.properties;
 import java.util.Hashtable;
 import java.util.List;
 
+import org.eclipse.bpmn2.Activity;
 import org.eclipse.bpmn2.BaseElement;
+import org.eclipse.bpmn2.CatchEvent;
+import org.eclipse.bpmn2.DataInput;
+import org.eclipse.bpmn2.DataInputAssociation;
+import org.eclipse.bpmn2.DataOutput;
+import org.eclipse.bpmn2.DataOutputAssociation;
 import org.eclipse.bpmn2.Definitions;
+import org.eclipse.bpmn2.InputOutputSpecification;
 import org.eclipse.bpmn2.ItemDefinition;
 import org.eclipse.bpmn2.RootElement;
+import org.eclipse.bpmn2.ThrowEvent;
 import org.eclipse.bpmn2.modeler.core.adapters.AdapterUtil;
 import org.eclipse.bpmn2.modeler.core.adapters.ExtendedPropertiesAdapter;
 import org.eclipse.bpmn2.modeler.core.adapters.FeatureDescriptor;
 import org.eclipse.bpmn2.modeler.core.utils.ModelUtil;
 import org.eclipse.core.runtime.Assert;
 import org.eclipse.emf.common.notify.AdapterFactory;
+import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EStructuralFeature;
@@ -78,10 +87,25 @@ public class ItemDefinitionRefFeatureDescriptor<T extends BaseElement> extends F
 	}
 
 	@Override
-	public void setValue(Object context, Object value) {
+	public void setValue(final Object context, final Object value) {
+		TransactionalEditingDomain editingDomain = getEditingDomain(object);
+		if (editingDomain == null) {
+			internalSetValue(context, value);
+		} else {
+			editingDomain.getCommandStack().execute(new RecordingCommand(editingDomain) {
+				@Override
+				protected void doExecute() {
+					internalSetValue(context, value);
+				}
+			});
+		}
+	}
+	
+	private void internalSetValue(Object context, Object value) {
 		T object = adopt(context);
+		ItemDefinition itemDefinition = null;
 		if (value instanceof ItemDefinition) {
-			ItemDefinition itemDefinition = (ItemDefinition) value;
+			itemDefinition = (ItemDefinition) value;
 			if (itemDefinition.eResource()==null) {
 				final Definitions defs = ModelUtil.getDefinitions(object);
 				if (defs!=null) {
@@ -104,6 +128,60 @@ public class ItemDefinitionRefFeatureDescriptor<T extends BaseElement> extends F
 		}
 		else if (value==null)
 			super.setValue(object, null);
+		
+		// if there are any DataInputAssociations or DataOutputAssociations that map to this object
+		// then change their ItemDefinitions to match.
+		Definitions definitions = ModelUtil.getDefinitions(object);
+		if (definitions!=null) {
+			TreeIterator<EObject> iter = definitions.eAllContents();
+			while (iter.hasNext()) {
+				EObject o = iter.next();
+				if (o instanceof DataInput) {
+					DataInput input = (DataInput) o;
+					if (input.eContainer() instanceof InputOutputSpecification) {
+						InputOutputSpecification ioSpec = (InputOutputSpecification) input.eContainer();
+						if (ioSpec.eContainer() instanceof Activity) {
+							Activity activity = (Activity) ioSpec.eContainer();
+							for (DataInputAssociation dia : activity.getDataInputAssociations()) {
+								if (!dia.getSourceRef().isEmpty() && dia.getSourceRef().get(0) == object) {
+									input.setItemSubjectRef(itemDefinition);
+								}
+							}
+						}
+					}
+					else if (input.eContainer() instanceof ThrowEvent) {
+						ThrowEvent event = (ThrowEvent) input.eContainer();
+						for (DataInputAssociation dia : event.getDataInputAssociation()) {
+							if (!dia.getSourceRef().isEmpty() && dia.getSourceRef().get(0) == object) {
+								input.setItemSubjectRef(itemDefinition);
+							}
+						}
+					}
+				}
+				else if (o instanceof DataOutput) {
+					DataOutput output = (DataOutput) o;
+					if (output.eContainer() instanceof InputOutputSpecification) {
+						InputOutputSpecification ioSpec = (InputOutputSpecification) output.eContainer();
+						if (ioSpec.eContainer() instanceof Activity) {
+							Activity activity = (Activity) ioSpec.eContainer();
+							for (DataOutputAssociation doa : activity.getDataOutputAssociations()) {
+								if (doa.getTargetRef() == object) {
+									output.setItemSubjectRef(itemDefinition);
+								}
+							}
+						}
+					}
+					else if (output.eContainer() instanceof CatchEvent) {
+						CatchEvent event = (CatchEvent) output.eContainer();
+						for (DataOutputAssociation doa : event.getDataOutputAssociation()) {
+							if (doa.getTargetRef() == object) {
+								output.setItemSubjectRef(itemDefinition);
+							}
+						}
+					}
+				}
+			}
+		}
 	}
 
 	@Override
