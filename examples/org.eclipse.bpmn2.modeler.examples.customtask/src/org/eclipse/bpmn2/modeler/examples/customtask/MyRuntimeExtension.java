253a639b920f99aba09d8857820acf2b077c02fe
Bob Brodt <bbrodt@redhat.com>
2014-01-14 09:28:17 -0700

    Fix NPE caused by null returned from example Target Runtime MyRuntimeExtension.getExpressionLanguages() and getTypeLanguages(). This needs to be documented: these methods must return a non-null array, or should subclass DefaultBpmn2RuntimeExtension.

--- examples/org.eclipse.bpmn2.modeler.examples.customtask/src/org/eclipse/bpmn2/modeler/examples/customtask/MyRuntimeExtension.java
+++ examples/org.eclipse.bpmn2.modeler.examples.customtask/src/org/eclipse/bpmn2/modeler/examples/customtask/MyRuntimeExtension.java
@@ -10,21 +10,16 @@
  ******************************************************************************/
 package org.eclipse.bpmn2.modeler.examples.customtask;
 
-import org.eclipse.bpmn2.modeler.core.IBpmn2RuntimeExtension;
-import org.eclipse.bpmn2.modeler.core.preferences.Bpmn2Preferences;
 import org.eclipse.bpmn2.modeler.core.utils.ModelUtil.Bpmn2DiagramType;
-import org.eclipse.bpmn2.modeler.ui.DefaultBpmn2RuntimeExtension.RootElementParser;
+import org.eclipse.bpmn2.modeler.ui.DefaultBpmn2RuntimeExtension;
 import org.eclipse.bpmn2.modeler.ui.wizards.FileService;
-import org.eclipse.emf.ecore.EObject;
 import org.eclipse.graphiti.ui.editor.DiagramEditor;
-import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.IEditorInput;
 import org.xml.sax.InputSource;
 
-public class MyRuntimeExtension implements IBpmn2RuntimeExtension {
+public class MyRuntimeExtension extends DefaultBpmn2RuntimeExtension {
 
 	public MyRuntimeExtension() {
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
@@ -38,23 +33,5 @@ public class MyRuntimeExtension implements IBpmn2RuntimeExtension {
 	@Override
 	public String getTargetNamespace(Bpmn2DiagramType diagramType) {
 		return "http://org.eclipse.bpmn2.modeler.examples.customtask";
-	}
-
-	@Override
-	public String[] getTypeLanguages() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public String[] getExpressionLanguages() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void initialize(DiagramEditor editor) {
-		// TODO Auto-generated method stub
-
 	}
 }
