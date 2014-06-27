4f74d2715a45d3b448001bd8d28ce5514bab46dd
Bob Brodt <bbrodt@redhat.com>
2014-06-27 11:12:04 -0600

    Fixed a bug in ModelEnablements for extension attributes

--- plugins/org.eclipse.bpmn2.modeler.core/src/org/eclipse/bpmn2/modeler/core/preferences/ModelEnablements.java
+++ plugins/org.eclipse.bpmn2.modeler.core/src/org/eclipse/bpmn2/modeler/core/preferences/ModelEnablements.java
@@ -389,10 +389,10 @@ public class ModelEnablements {
 				EClass eClass = getEClass(className);
 				if (eClass!=null) {
 					for (EClass st : eClass.getEAllSuperTypes()) {
-						className = st.getName();
-						if (classes.containsKey(className)) { // && isOverride()) {
+						String cn = st.getName();
+						if (classes.containsKey(cn)) { // && isOverride()) {
 							if (featureName!=null && !featureName.isEmpty()) {
-								features = classes.get(className);
+								features = classes.get(cn);
 								if (features.contains(featureName))
 									return true;
 								break;
