a1a213ae6807f3c52642c54f1f723efab8e73a48
Bob Brodt <bbrodt@redhat.com>
2014-09-24 14:23:58 -0600

    https://bugzilla.redhat.com/show_bug.cgi?id=1144961 - "Script language" for Actors and I/O Parameters

--- org.eclipse.bpmn2.modeler.runtime.jboss.jbpm5/src/org/eclipse/bpmn2/modeler/runtime/jboss/jbpm5/property/adapters/JbpmFormalExpressionPropertiesAdapter.java
+++ org.eclipse.bpmn2.modeler.runtime.jboss.jbpm5/src/org/eclipse/bpmn2/modeler/runtime/jboss/jbpm5/property/adapters/JbpmFormalExpressionPropertiesAdapter.java
@@ -17,8 +17,10 @@ import java.util.Hashtable;
 
 import org.eclipse.bpmn2.Bpmn2Package;
 import org.eclipse.bpmn2.FormalExpression;
+import org.eclipse.bpmn2.ResourceAssignmentExpression;
 import org.eclipse.bpmn2.SequenceFlow;
 import org.eclipse.bpmn2.modeler.core.adapters.FeatureDescriptor;
+import org.eclipse.bpmn2.modeler.core.adapters.ObjectDescriptor;
 import org.eclipse.bpmn2.modeler.core.runtime.TargetRuntime;
 import org.eclipse.bpmn2.modeler.ui.adapters.properties.FormalExpressionPropertiesAdapter;
 import org.eclipse.emf.common.notify.AdapterFactory;
@@ -64,6 +66,16 @@ public class JbpmFormalExpressionPropertiesAdapter extends FormalExpressionPrope
 				
 			}
     	);
+    	
+    	setObjectDescriptor(new ObjectDescriptor<FormalExpression>(adapterFactory, object) {
+			@Override
+			public String getLabel(Object context) {
+				FormalExpression object = adopt(context);
+				if (object.eContainer() instanceof ResourceAssignmentExpression)
+					return Messages.JbpmFormalExpressionPropertiesAdapter_Actor;
+				return Messages.JbpmFormalExpressionPropertiesAdapter_Expression;
+			}
+    	});
 	}
 
 }
--- org.eclipse.bpmn2.modeler.runtime.jboss.jbpm5/src/org/eclipse/bpmn2/modeler/runtime/jboss/jbpm5/property/adapters/Messages.java
+++ org.eclipse.bpmn2.modeler.runtime.jboss.jbpm5/src/org/eclipse/bpmn2/modeler/runtime/jboss/jbpm5/property/adapters/Messages.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2011, 2012, 2013, 2014 Red Hat, Inc.
+ * All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
 package org.eclipse.bpmn2.modeler.runtime.jboss.jbpm5.property.adapters;
 
 import org.eclipse.osgi.util.NLS;
@@ -10,6 +20,8 @@ public class Messages extends NLS {
 	public static String JbpmGlobalTypePropertiesAdapter_Data_Type_Label;
 	public static String JbpmGlobalTypePropertiesAdapter_Label;
 	public static String JbpmGlobalTypePropertiesAdapter_Name;
+	public static String JbpmFormalExpressionPropertiesAdapter_Actor;
+	public static String JbpmFormalExpressionPropertiesAdapter_Expression;
 	static {
 		// initialize resource bundle
 		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
