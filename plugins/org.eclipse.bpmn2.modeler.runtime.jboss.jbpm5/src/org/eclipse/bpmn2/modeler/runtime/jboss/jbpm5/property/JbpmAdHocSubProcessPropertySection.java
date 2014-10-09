c9bb2cc0d7a662b05cbc55a79f4925d4279312d1
Bob Brodt <bbrodt@redhat.com>
2014-10-09 13:11:06 -0600

    Bug 446154 - Unable to set onEntry/onExit script for embedded AdHocSubprocess

---
+++ plugins/org.eclipse.bpmn2.modeler.runtime.jboss.jbpm5/src/org/eclipse/bpmn2/modeler/runtime/jboss/jbpm5/property/JbpmAdHocSubProcessPropertySection.java
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ * Copyright (c) 2011, 2012 Red Hat, Inc.
+ *  All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Red Hat, Inc. - initial API and implementation
+ *
+ * @author Bob Brodt
+ ******************************************************************************/
+
+package org.eclipse.bpmn2.modeler.runtime.jboss.jbpm5.property;
+
+import org.eclipse.bpmn2.Bpmn2Package;
+import org.eclipse.bpmn2.modeler.core.merrimac.clad.AbstractDetailComposite;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.swt.widgets.Composite;
+
+/**
+ * @author Bob Brodt
+ *
+ */
+public class JbpmAdHocSubProcessPropertySection extends JbpmActivityPropertySection {
+
+	@Override
+	protected AbstractDetailComposite createSectionRoot() {
+		return new JbpmAdHocSubProcessDetailComposite(this);
+	}
+
+	@Override
+	public AbstractDetailComposite createSectionRoot(Composite parent, int style) {
+		return new JbpmAdHocSubProcessDetailComposite(parent,style);
+	}
+
+	@Override
+	public boolean appliesTo(EObject eObj) {
+		return eObj.eClass() == Bpmn2Package.eINSTANCE.getAdHocSubProcess();
+	}
+}
