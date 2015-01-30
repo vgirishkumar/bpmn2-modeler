86f98e4f509a596aff654a6d9d0d5bdf5f74f7a0
Bob Brodt <bbrodt@redhat.com>
2015-01-30 11:07:34 -0700

    Upversioned to 1.0.4

--- upversion.sh
+++ upversion.sh
@@ -1,1 +1,1 @@
-mvn -Dtycho.mode=maven org.sonatype.tycho:tycho-versions-plugin:set-version -DnewVersion=1.0.3-SNAPSHOT
+mvn -Dtycho.mode=maven org.sonatype.tycho:tycho-versions-plugin:set-version -DnewVersion=1.0.4-SNAPSHOT
