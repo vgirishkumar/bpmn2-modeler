f6d25780918ba1ffd782004f03be9f7bc8024e91
Bob Brodt <bbrodt@redhat.com>
2015-03-02 09:56:49 -0700

    Created version 1.1.3 for next release

--- upversion.sh
+++ upversion.sh
@@ -1,1 +1,1 @@
-mvn -Dtycho.mode=maven org.sonatype.tycho:tycho-versions-plugin:set-version -DnewVersion=1.1.2.Final
+mvn -Dtycho.mode=maven org.sonatype.tycho:tycho-versions-plugin:set-version -DnewVersion=1.1.3-SNAPSHOT
