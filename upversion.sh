62b260909dd29cd5b0a7755be2ff979be6ecd5ba
Bob Brodt <bbrodt@redhat.com>
2015-05-15 13:27:46 -0600

    Upversion to 1.1.3.Final

--- upversion.sh
+++ upversion.sh
@@ -1,1 +1,1 @@
-mvn -Dtycho.mode=maven org.sonatype.tycho:tycho-versions-plugin:set-version -DnewVersion=1.1.3-SNAPSHOT
+mvn -Dtycho.mode=maven org.sonatype.tycho:tycho-versions-plugin:set-version -DnewVersion=1.1.3.Final
