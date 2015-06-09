a668bf8e9505991b438b6101bfb72d5afc5bb7cf
Bob Brodt <bbrodt@redhat.com>
2015-06-09 14:42:33 -0600

    Upversioned to 1.1.4 SNAPSHOT for next SR

--- upversion.sh
+++ upversion.sh
@@ -1,1 +1,1 @@
-mvn -Dtycho.mode=maven org.sonatype.tycho:tycho-versions-plugin:set-version -DnewVersion=1.1.3.Final
+mvn -Dtycho.mode=maven org.sonatype.tycho:tycho-versions-plugin:set-version -DnewVersion=1.1.4.qualifier
