52c4da16369fc70f9a2691c28a160d64edd9875e
Bob Brodt <bbrodt@redhat.com>
2015-11-09 12:51:20 -0700

    https://bugzilla.redhat.com/show_bug.cgi?id=1249658 - Input parameters of throw event affect process variables

--- plugins/org.eclipse.bpmn2.modeler.runtime.jboss.jbpm5/src/org/eclipse/bpmn2/modeler/runtime/jboss/jbpm5/property/adapters/JbpmDataOutputPropertiesAdapter.java
+++ plugins/org.eclipse.bpmn2.modeler.runtime.jboss.jbpm5/src/org/eclipse/bpmn2/modeler/runtime/jboss/jbpm5/property/adapters/JbpmDataOutputPropertiesAdapter.java
@@ -14,6 +14,9 @@ import java.util.Hashtable;
 
 import org.eclipse.bpmn2.Bpmn2Package;
 import org.eclipse.bpmn2.DataOutput;
+import org.eclipse.bpmn2.ItemAwareElement;
+import org.eclipse.bpmn2.ItemDefinition;
+import org.eclipse.bpmn2.RootElement;
 import org.eclipse.bpmn2.modeler.runtime.jboss.jbpm5.util.JbpmModelUtil;
 import org.eclipse.bpmn2.modeler.ui.adapters.properties.DataOutputPropertiesAdapter;
 import org.eclipse.bpmn2.modeler.ui.adapters.properties.ItemDefinitionRefFeatureDescriptor;
@@ -38,7 +41,17 @@ public class JbpmDataOutputPropertiesAdapter extends DataOutputPropertiesAdapter
 	    			value = JbpmModelUtil.getDataType(dataOutput, value);
 	    			super.internalSet(object, feature, value, index);
 				}
-				
+	    		
+	    		@Override
+				protected void changeReferences(RootElement object, ItemDefinition itemDefinition) {
+	    			// do nothing!
+	    		}
+	    		
+	    		@Override
+				protected void changeReferences(ItemAwareElement object, ItemDefinition itemDefinition) {
+	    			// do nothing!
+	    		}
+
 				@Override
 				public Hashtable<String, Object> getChoiceOfValues() {
 					return JbpmModelUtil.getChoiceOfValues(object);
